6a901a77461034dbae401263b5fe06fb74609ebc
AlekG25 <alekgolubchik@gmail.com>
2024-01-31 10:22:21 -0800

    TeachableMachine working
    The teachable machine can be uppercase and no space, no description needed.

---
+++ static/sketch.js
@@ -0,0 +1,80 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Webcam Image Classification using a pre-trained customized model and p5.js
+This example uses p5 preload function to create the classifier
+=== */
+
+// Classifier Variable
+let classifier;
+let img;
+// Model URL
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/PQYf__Udr/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  img = loadImage('POLSKA!!!.jpeg');
+}
+
+function setup() {
+  createCanvas(320, 260);
+  img.resize(370, 0);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
+
+  flippedVideo = ml5.flipImage(video)
+  // Start classifying
+  classifyVideo();
+}
+
+function draw() {
+  background(0);
+  // Draw the video
+if (label == 'Złoty'){
+    ZłotyImage = loadImage('POLSKA!!!.jpeg');
+    image(ZłotyImage, 0, 0);
+   image(img, 0, 0);
+  }
+  else{
+  image(flippedVideo, 0, 0);
+
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+  }
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  // Classifiy again!
+  classifyVideo();
+}
